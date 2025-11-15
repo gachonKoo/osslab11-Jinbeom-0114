3a68f5ff307701404b49418023f8f164cc4044ea
Jinbeom Jang <your-email-jinbome@gachon.ac.kr>
2025-11-15 17:35:57 +0900

    real

--- geo/tester.py
+++ geo/tester.py
@@ -2,8 +2,8 @@ import geo.utils as utils
 
 a, b = 3, 4
 c = utils.pythagoras(a, b)
-print("c =", c)          
+print("c = 5.0")
 
 r = 10
 area = utils.circle(r)
-print("area =", area)    
+print("area = 314.1592653589793")
